c3bc217d2d1544c60b874f0d952a04597914d929
Cytical <guiaomikhail@gmail.com>
2022-08-11 21:42:10 +0800

    fetching api

--- src/Menu.js
+++ src/Menu.js
@@ -1,19 +1,20 @@
 import React, { useEffect, useState } from 'react'
 import useAuth from './useAuth'
 import SpotifyWebApi from 'spotify-web-api-node'
-import { Container, Form } from "react-bootstrap"
 
 const spotifyApi = new SpotifyWebApi({
   clientId: 'ae3f94d228c64ca69dfb58b533b679ef',
 })
 
-
 export default function Menu({ code }) {
   
   const accessToken = useAuth(code);
 
-  const [search, setSearch] = useState("");
-  const [searchResults, setSearchResults] = useState([]);
+  const [username, setUsername] = useState("");
+  const [user_albums, setUser_albums] = useState([]);
+  const [user_tracks, setUser_tracks] = useState([]);
+  
+
 
   useEffect(() => {
     if (!accessToken) return
@@ -21,66 +22,37 @@ export default function Menu({ code }) {
   }, [accessToken])
 
   useEffect(() => {
-    if (!search) return setSearchResults([])
     if (!accessToken) return
+  // Get the authenticated user
+  spotifyApi.getMe()
+    .then(function(data) {
+      const username = data.body.display_name;
+      setUsername(username)
 
-    // console.log(searchResults)
-    try {
-      console.log(searchResults['0']['artist'], searchResults['0']['title'])
-      console.log()
-    }
-    catch (err) {
-      console.log(err)
-    }
+    }, function(err) {
+      console.log('Something went wrong!', err);
+    });
+
+  // Get tracks in the signed in user's Your Music library
+  spotifyApi.getMySavedTracks({
+      limit : 50,
+      offset: 1
+    })
+    .then(function(data) {
+      data.body.items.map(track => {
+        const track_info = track
+        console.log(track.track.name)
+      })
+    }, function(err) {
+      console.log('Something went wrong!', err);
+    });
 
 
-  //   let cancel = false
-  //   spotifyApi.searchTracks(search).then(res => {
-  //     if (cancel) return
-  //     setSearchResults(
-  //       res.body.tracks.items.map(track => {
-  //         const smallestAlbumImage = track.album.images.reduce(
-  //           (smallest, image) => {
-  //             if (image.height < smallest.height) return image
-  //             return smallest
-  //           },
-  //           track.album.images[0]
-  //         )
-
-  //         return {
-  //           artist: track.artists[0].name,
-  //           title: track.name,
-  //           uri: track.uri,
-  //           albumUrl: smallestAlbumImage.url,
-  //         }
-  //       })
-  //     )
-  //   })
-
-  //   return () => (cancel = true)
-  // }, [search, accessToken])
-  
-  spotifyApi.searchTracks('v').then(res => {
-    const temp = res.body.tracks.items
-    console.log(temp)
-  })
-  }, [search, accessToken])
+  }, [accessToken])
 
   
   return (
-     <Container className="d-flex flex-column py-2" style={{ height: "100vh" }}>
-      <Form.Control
-        type="search"
-        placeholder="Search Songs/Artists"
-        value={search}
-        onChange={e => setSearch(e.target.value)}
-      />
-      <div className="flex-grow-1 my-2" style={{ overflowY: "auto" }}>
-        {JSON.stringify(searchResults['0'])}
-      </div>
-      {/* <div>
-        {searchResults}
-      </div> */}
-    </Container>
+    <div>{username} </div> 
+
   )
 }
