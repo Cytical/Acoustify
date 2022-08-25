8a36565f1a4b651bfc55ae48cdfd67336eda35cf
Cytical <guiaomikhail@gmail.com>
2022-08-25 20:21:32 +0800

    heroku

--- src/useAuth.js
+++ src/useAuth.js
@@ -14,7 +14,7 @@ export default function Auth(code) {
             setAccessToken(res.data.accessToken)
             setRefreshToken(res.data.refreshToken)
             setExpiresIn(res.data.expiresIn)
-            window.history.pushState({}, null, '/')
+            // window.history.pushState({}, null, '/')
         }).catch((err) => {
             console.log(err)
             window.location = '/'
