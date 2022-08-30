ed032335c8ba5853ed62ccf9be2600c723d35b83
Cytical <guiaomikhail@gmail.com>
2022-08-30 09:12:02 +0800

    a

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3001;
 if(process.env.NODE_ENV === "production") {
     app.use(express.static('build'))
     app.get('*', (req,res) => {
-        req.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
     })
 }
 
